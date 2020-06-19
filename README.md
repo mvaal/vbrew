e67306f8dfd2969d28ec86914e2cedccb0dfd7f6
mvaal <mjvaal@gmail.com>
2020-06-19 16:04:56 -0500

    Initial commit
    vbrew.sh script

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # vbrew
-Allows you to install older brew formulas
+Script that should help alleviate pain of installing older brew formulas.
+
+## Why vbrew?
+`brew` does not currently support the installation of older formula versions. 
+This is a solution to that problem.
+Additionally, older formula versions are using a SHA256 that might no longer work
+for the current version of your OS. `vbrew` will correct the SHA256 should this happen.
+
+## First time use
+Assumes you are using SSH to clone, https support needs to be added.
+
+Clone `vbrew`
+
+    git clone git@github.com:mvaal/vbrew.git vbrew
+
+Run
+
+    ./vbrew.sh
+
+Add `vbrew` alias to your profile. The alias will print after first run.
+
+    alias vbrew="<repo-path>/vbrew.sh"
+
+## Usage
+Other than installing specific formula versions, vbrew proxies all other calls to `brew` so you
+can use it as if you were using `brew` itself
+
+    vbrew install terraform 0.12.24
+    vbrew uninstall terraform # Same as calling brew uninstall terraform
+
+Running the `install` command will automatic switch to the older version.
---
+++ vbrew.sh
@@ -0,0 +1,58 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR" || exit
+if [ ! -d "homebrew-core" ]; then
+  git clone git@github.com:Homebrew/homebrew-core.git homebrew-core
+  printf 'Please add vbrew.sh to your profile.\nalias vbrew="%s"\n' "$DIR/vbrew.sh"
+fi
+
+COMMAND=$1
+FORMULA=$2
+VERSION=$3
+
+if [ "$COMMAND" != "install" ] || [ -z "$VERSION" ] ; then
+  echo "Proxying call to brew..."
+  brew "$@"
+  exit 0
+fi
+if [ -z "$FORMULA" ]; then
+  echo Please provide formula
+  exit 1
+fi
+
+cd homebrew-core || exit
+
+git fetch && git switch -C master origin/master
+
+REVISION=$(git log --all --grep="$FORMULA $VERSION" | grep commit | sed 's/^.* //')
+if [ -z "$REVISION" ]; then
+  echo "$FORMULA-$VERSION not found in homebrew-core"
+  exit 1
+fi
+git checkout -b "${FORMULA}-${VERSION}" 2>/dev/null || git checkout -B "${FORMULA}-${VERSION}" "$REVISION"
+
+FORMULA_PATH="Formula/$FORMULA.rb"
+
+brew unlink "$FORMULA"
+
+INSTALL_RESULT=$(brew install "$FORMULA_PATH" 2>&1)
+SIGINT=$?
+if [ $SIGINT -ne 0 ]; then
+  ERROR=$(echo "$INSTALL_RESULT" | awk -F': ' '/[Ee]rror/ {print $2}')
+  if [ "$ERROR" == "SHA256 mismatch" ]; then
+    echo "SHA256 mismatch, correcting..."
+    EXPECTED=$(echo "$INSTALL_RESULT" | awk -F': ' '/[Ee]xpected/ {print $2}')
+    ACTUAL=$(echo "$INSTALL_RESULT" | awk -F': ' '/[Aa]ctual/ {print $2}')
+    sed -i -e "s/$EXPECTED/$ACTUAL/g" "$FORMULA_PATH"
+    git add "$FORMULA_PATH"
+    git commit -m "Updating sha256 to $ACTUAL"
+    brew install "$FORMULA_PATH"
+  fi
+else
+  WARNING=$(echo "$INSTALL_RESULT" | awk -F': ' '/[Ww]arning/ {print $2}')
+  if [ "$WARNING" == "$FORMULA $VERSION is already installed, it's just not linked" ]; then
+    brew switch "$FORMULA" "$VERSION"
+  else
+    echo "$INSTALL_RESULT"
+    exit $SIGINT
+  fi
+fi
